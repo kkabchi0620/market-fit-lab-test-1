6733cd62f2da84ae4d0b3af1581c50a5d08fde59
hyjang0620 <onlinekkabchiaws@gmail.com>
2024-08-03 09:40:34 +0900

    feat: vite app listening on all interfaces

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "vite-app",
       script: "serve",
-      args: "-s dist",
+      args: "-s dist -l tcp://0.0.0.0:3000",
       env: {
         PORT: 3000,
         NODE_ENV: "production",
