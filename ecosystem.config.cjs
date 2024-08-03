e175fa434cae393a2100ced484f46fc599a518e0
hyjang0620 <onlinekkabchiaws@gmail.com>
2024-08-03 09:43:35 +0900

    fix: 포트번호 변경

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "vite-app",
       script: "serve",
-      args: "-s dist -l tcp://0.0.0.0:3000",
+      args: "-s dist -l 0.0.0.0:8080",
       env: {
         PORT: 3000,
         NODE_ENV: "production",
